223242cf897268d85048c6568449735f972eb041
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2024-02-08 01:01:31 -0500

    Update models.py

--- crea/admin/models.py
+++ crea/admin/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class propiedad(models.Model):
+    tipos = {
+        "Casa": "Casa",
+        "Terreno": "Terreno",
+    }
+    tipo = models.CharField(max_length=15, choices=tipos)
+    ubicacion = models.CharField(max_length=144, blank= False, null= False)
+    precio = models.DecimalField(blank = False, null = False)
+    descripcion = models.TextField(max_length=500, blank= False, null= False)
+    #image = models.
+    estados = {
+            "Disponible": "Disponible",
+            "Vendida": "Vendida",
+            "Reservado": "Reservado",
+        }
+    estado = models.CharField(max_length=25, choices=estados)
+    caracteristicas = models.ManyToManyField(caracteristicas)
+    servicios = models.ManyToManyField(servicios)
+
+    def __str__(self) -> str:
+       return f'{self.tipo}'
+    
+class caracteristicas(models.Model):
+    nombre = models.CharField(max_length=144, blank= False, null= False)
+    descripcion = models.CharField(max_length=144, blank= False, null= False)
+
+    def __str__(self) -> str:
+       return f'{self.nombre}'
+
+class servicios(models.Model):
+    nombre = models.CharField(max_length=144, blank= False, null= False)
+    descripcion = models.CharField(max_length=144, blank= False, null= False)
+
+    def __str__(self) -> str:
+       return f'{self.nombre}'
