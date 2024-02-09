008dc2be9bd9b0dee603a178179009cb734863b1
miguelv1261 <99453270+miguelv1261@users.noreply.github.com>
2024-02-08 21:37:00 -0500

    Models, Views, Templates

--- crea/crea/urls.py
+++ crea/crea/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from crea_admin.views import ver_cliente, ver_propiedades, ver_propiedad
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cliente/', ver_cliente, name="cliente"),
+    path('propiedades/', ver_propiedades),
+    path('propiedad/<int:codigo_propiedad>/', ver_propiedad ,name="detalle_propiedad")
 ]
--- crea/crea_admin/views.py
+++ crea/crea_admin/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from .models import Cliente, Propiedad
+
+def ver_cliente(request):
+    cliente = Cliente.objects.all()
+    
+    contenido ={
+        'cliente': cliente,
+    }
+    template = "index.html"
+    return render(request, template, contenido)
+    
+def ver_propiedades(request):
+    propiedad = Propiedad.objects.all()
+    contenido = {
+        'propiedad' : propiedad
+    }
+    template = "propiedades.html"
+    return render(request, template, contenido)
+
+def ver_propiedad(request, codigo_propiedad):
+    propiedad = Propiedad.objects.get(pk = codigo_propiedad)
+    contenido = {
+        'propiedad' : propiedad
+    }
+    template = "propiedad.html"
+    return render(request, template, contenido)
