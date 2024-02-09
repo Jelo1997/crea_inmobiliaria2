ebf010a5fb366df3403faf01bfc32ea2976e5965
Jelo1997 <95892392+Jelo1997@users.noreply.github.com>
2024-02-08 23:44:15 -0500

    model_emplea2

--- crea/crea_admin/admin.py
+++ crea/crea_admin/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Propiedad, Caracteristicas, Servicios, Cliente
+from .models import Propiedad, Caracteristicas, Servicios, Cliente, Empleado
 # Register your models here.
 
 @admin.register(Propiedad)
@@ -17,3 +17,6 @@ class ServiciosAdmin(admin.ModelAdmin):
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     list_display = ('id', 'nombre')
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username')
